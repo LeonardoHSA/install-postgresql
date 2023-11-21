7fb57da305df3ff010543513837d9ad0c8012e9f
Leonardo Sanfins <leosanfinsh@gmail.com>
2023-11-21 13:43:31 -0300

    Alterando automação

--- install-postgresql.sh
+++ install-postgresql.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Atualizando o sistema..."
+yum update -y
+
 echo "### Verificando versões do postgresql ###"
 
 yum module list postgresql
@@ -11,19 +14,21 @@ read version
 echo ""
 
 echo "Configurando a versão selecionada como padrão"
-yum module enable postgresql:$version
+yum module enable postgresql:$version -y
 echo ""
 
 echo "Instalando..."
-yum install postgresql-server
+yum install postgresql-server -y
 echo ""
 
 echo "Iniciando e habilitando o serviço no boot..."
 #Verificando se o diretório /var/lib/pgsql/data/ está vazio
-if [" $ (ls -A /var/lib/pgsql/data/pg_hba.conf)"]
+if [ -d /var/lib/pgsql/data/* ]
 then
-	echo "Postgre já está funcionando"
+	# Se o diretório não estiver vazio
+	echo "O postgresql já está funcionando!!!"
 else
+	# Se o diretório estiver vazio
 	service postgresql initdb
 	systemctl start postgresql
 	systemctl enable postgresql
@@ -34,7 +39,9 @@ echo "Configurando o postgresql para acesso remoto..."
 echo "------"
 echo "Alterando o arquivo pg_hba.conf"
 sed -i 's/127.0.0.1\/32/0.0.0.0\/0/g' /var/lib/pgsql/data/pg_hba.conf
-sed -1 's/ident/trust/g' /var/lib/pgsql/data/pg_hba.conf
-
-
-
+sed -i 's/ident/trust/g' /var/lib/pgsql/data/pg_hba.conf
+echo "------"
+echo "Alterando o arquivo /var/lib/pgsql/data/postgresql.conf"
+sed -i 's/#listen_addresses/listen_addresses/g' /var/lib/pgsql/data/postgresql.conf
+sed -i 's/localhost/\*/g' /var/lib/pgsql/data/postgresql.conf
+sed -i 's/#port = 5432/port = 5432/g' /var/lib/pgsql/data/postgresql.conf
